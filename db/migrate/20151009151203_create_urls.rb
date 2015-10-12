0eb7a0b17bd1fbb16f3822028ffd03474af493da
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-10-12 09:46:55 -0400

    migration for urls

--- db/migrate/20151009151203_create_urls.rb
+++ db/migrate/20151009151203_create_urls.rb
@@ -2,8 +2,9 @@ class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
       t.string :full_url, null: false
-      t.string :short_code, null: false
+      t.string :short_code
       t.integer :access_count, null: false, default: 0
+      t.references :user
 
       t.timestamps
     end
